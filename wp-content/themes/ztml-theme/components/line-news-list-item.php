473003602fa27ae01dcd100be5b47dd562abef62
KozakevichDzmitry <kozakevichdm@gmail.com>
2022-09-01 18:17:01 +0300

    add share btn in 'avtorskaya-kolonka'

--- wp-content/themes/ztml-theme/components/line-news-list-item.php
+++ wp-content/themes/ztml-theme/components/line-news-list-item.php
@@ -25,11 +25,10 @@ function render_line_news_list_item($post_ID, $reversClass = false)
 					<span><?php echo get_the_time('H:i', $post_ID); ?></span>
 					<span><?php echo get_the_time('d.m.Y', $post_ID); ?></span>
 				</div>
-				<div class="info__share-btn">
-					<svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M9.33333 0L8.23667 1.00714L9.33333 2.02143L11.0211 3.57143H3.88889C1.74222 3.57143 0 5.17143 0 7.14286V10H1.55556V7.14286C1.55556 5.96429 2.60556 5 3.88889 5H11.0211L8.23667 7.55714L9.33333 8.57143L14 4.28571L9.33333 0Z" fill="#D91E23" fill-opacity="0.6" />
-					</svg>
-				</div>
+                <div class="share-block--fold">
+                    <?php echo do_shortcode('[share_links]'); ?>
+                    <?php render_share_icon(); ?>
+                </div>
 			</div>
 		</div>
 	</div>
