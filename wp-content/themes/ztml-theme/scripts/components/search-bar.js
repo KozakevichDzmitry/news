ae80f78a029bf189d071da92802c7c21d0849cbc
KozakevichDzmitry <kozakevichdm@gmail.com>
2022-09-02 17:29:55 +0300

    disable scroll when search-bar is open

--- wp-content/themes/ztml-theme/scripts/components/search-bar.js
+++ wp-content/themes/ztml-theme/scripts/components/search-bar.js
@@ -1,15 +1,17 @@
-window.onload=function(){
-
-    var btnClose = document.getElementById("searchBtnClose");
-    var btnOpen = document.getElementById("search-btn");
+jQuery(document).ready(function(){
+    var btnOff = document.getElementById("searchBtnClose");
+    var btnOn = document.getElementById("search-btn");
     var searchBar = document.getElementById("search-bar");
-    btnOpen.addEventListener('click', openCloseSearch)
-    btnClose.addEventListener('click', openCloseSearch)
+    var body = document.body;
+    btnOn.addEventListener('click', openCloseSearch)
+    btnOff.addEventListener('click', openCloseSearch)
     function openCloseSearch(){
         if(this.id === "searchBtnClose" ){
+            document.body.classList.remove('disable-scroll');
             searchBar.classList.remove('active')
         } else if(this.id === "search-btn"){
+            document.body.classList.add('disable-scroll');
             searchBar.classList.add('active')
         }
     }
-}
+})
